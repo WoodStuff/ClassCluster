1eff4cd57ab2e2432e6279ec7610297277f94398
TheEgglet <woodstuffthings@gmail.com>
2025-01-25 16:44:46 +0100

    add data rows to point tests

--- ClassClusterTests/PointTests.cs
+++ ClassClusterTests/PointTests.cs
@@ -4,79 +4,59 @@
 public class PointTests
 {
 	#region Constructor Tests
-	[TestMethod]
-	public void Constructor_InitializesProperties()
+	[DataTestMethod]
+	[DataRow(1, 2.5)]
+	[DataRow(-5.2, 0)]
+	public void Constructor_InitializesProperties(double x, double y)
 	{
-		Point p1 = new(1, 2.5);
-		Assert.AreEqual(1, p1.X);
-		Assert.AreEqual(2.5, p1.Y);
+		Point p1 = new(x, y);
+		Assert.AreEqual(x, p1.X);
+		Assert.AreEqual(y, p1.Y);
 	}
 
-	[TestMethod]
-	public void TupleConstruction_InitializesProperties()
+	[DataTestMethod]
+	[DataRow(1, 2.5)]
+	[DataRow(-5.2, 0)]
+	public void TupleConstruction_InitializesProperties(double x, double y)
 	{
-		Point p1 = (1, 2.5);
-		Assert.AreEqual(1, p1.X);
-		Assert.AreEqual(2.5, p1.Y);
+		Point p1 = (x, y);
+		Assert.AreEqual(x, p1.X);
+		Assert.AreEqual(y, p1.Y);
 	}
 
-	[TestMethod]
-	public void VectorConstruction_InitializesProperties()
+	[DataTestMethod]
+	[DataRow(1, 2.5)]
+	[DataRow(-5.2, 0)]
+	public void VectorConstruction_InitializesProperties(double x, double y)
 	{
-		Vector v1 = new(1, 2.5);
+		Vector v1 = new(x, y);
 		Point p1 = (Point)v1;
-		Assert.AreEqual(1, p1.X);
-		Assert.AreEqual(2.5, p1.Y);
+		Assert.AreEqual(x, p1.X);
+		Assert.AreEqual(y, p1.Y);
 	}
 	#endregion
 
 	#region Property Tests
-	[TestMethod]
-	public void DistanceFromOrigin_ReturnsCorrectDistance_AtPositiveCoordinates()
+	[DataTestMethod]
+	[DataRow(3, 4, 5)]
+	[DataRow(-3, -4, 5)]
+	[DataRow(0, 5, 5)]
+	public void DistanceFromOrigin_ReturnsCorrectDistance(double x, double y, double expected)
 	{
-		Point p1 = new(3, 4);
+		Point p1 = new(x, y);
 		double distance = p1.DistanceFromOrigin;
-		Assert.AreEqual(5, distance);
+		Assert.AreEqual(expected, distance);
 	}
 
-	[TestMethod]
-	public void DistanceFromOrigin_ReturnsCorrectDistance_AtNegativeCoordinates()
+	[DataTestMethod]
+	[DataRow(3, 6, 9)]
+	[DataRow(-3, -6, 9)]
+	[DataRow(9, 0, 9)]
+	public void GridDistFromOrigin_ReturnsCorrectDistance(double x, double y, double expected)
 	{
-		Point p1 = new(-3, -4);
-		double distance = p1.DistanceFromOrigin;
-		Assert.AreEqual(5, distance);
-	}
-
-	[TestMethod]
-	public void DistanceFromOrigin_ReturnsCorrectDistance_WhenPointIsOnAxis()
-	{
-		Point p1 = new(0, 5);
-		double distance = p1.DistanceFromOrigin;
-		Assert.AreEqual(5, distance);
-	}
-
-	[TestMethod]
-	public void GridDistFromOrigin_ReturnsCorrectDistance_AtPositiveCoordinates()
-	{
-		Point p1 = new(3, 6);
+		Point p1 = new(x, y);
 		double distance = p1.GridDistFromOrigin;
-		Assert.AreEqual(9, distance);
-	}
-
-	[TestMethod]
-	public void GridDistFromOrigin_ReturnsCorrectDistance_AtNegativeCoordinates()
-	{
-		Point p1 = new(-3, -6);
-		double distance = p1.GridDistFromOrigin;
-		Assert.AreEqual(9, distance);
-	}
-
-	[TestMethod]
-	public void GridDistFromOrigin_ReturnsCorrectDistance_WhenPointIsOnAxis()
-	{
-		Point p1 = new(9, 0);
-		double distance = p1.GridDistFromOrigin;
-		Assert.AreEqual(9, distance);
+		Assert.AreEqual(expected, distance);
 	}
 	#endregion
 
@@ -106,20 +86,14 @@ public class PointTests
 	#endregion
 
 	#region Operator Tests
-	[TestMethod]
-	public void Addition_ReturnsCorrectSum()
+	[DataTestMethod]
+	[DataRow(1, 1.5, 3.2, 6)]
+	[DataRow(1, -7.2, -4.5, 8)]
+	public void Addition_ReturnsCorrectSum(double x1, double y1, double x2, double y2)
 	{
-		Point p1 = new(1, 1.5);
-		Point p2 = new(3.2, 6);
-		Assert.AreEqual(new(4.2, 7.5), p1 + p2);
-	}
-
-	[TestMethod]
-	public void Addition_HandlesNegativeCoordinatesProperly()
-	{
-		Point p1 = new(1, -7.2);
-		Point p2 = new(-4.5, 8);
-		Assert.AreEqual(new(-3.5, 0.8), p1 + p2);
+		Point p1 = new(x1, y1);
+		Point p2 = new(x2, y2);
+		Assert.AreEqual(new(x1 + x2, y1 + y2), p1 + p2);
 	}
 
 	[TestMethod]
@@ -130,20 +104,14 @@ public class PointTests
 		Assert.AreEqual(p1, Point.Origin + p1);
 	}
 
-	[TestMethod]
-	public void Subtraction_ReturnsCorrectDifference()
+	[DataTestMethod]
+	[DataRow(5, 7, 1.5, 4)]
+	[DataRow(5.5, 6, 7, -2.2)]
+	public void Subtraction_ReturnsCorrectDifference(double x1, double y1, double x2, double y2)
 	{
-		Point p1 = new(5, 7);
-		Point p2 = new(1.5, 4);
-		Assert.AreEqual(new(3.5, 3), p1 - p2);
-	}
-
-	[TestMethod]
-	public void Subtraction_HandlesNegativeCoordinatesProperly()
-	{
-		Point p1 = new(5.5, 6);
-		Point p2 = new(7, -2.2);
-		Assert.AreEqual(new(-1.5, 8.2), p1 - p2);
+		Point p1 = new(x1, y1);
+		Point p2 = new(x2, y2);
+		Assert.AreEqual(new(x1 - x2, y1 - y2), p1 - p2);
 	}
 
 	[TestMethod]
@@ -155,12 +123,14 @@ public class PointTests
 		Assert.AreEqual(p1, result);
 	}
 
-	[TestMethod]
-	public void Inverse_WorksProperly()
+	[DataTestMethod]
+	[DataRow(5.5, -2)]
+	[DataRow(-4, 0)]
+	public void Inverse_WorksProperly(double x, double y)
 	{
-		Point p1 = new(5.5, -2);
+		Point p1 = new(x, y);
 		Point result = -p1;
-		Assert.AreEqual(new(-5.5, 2), result);
+		Assert.AreEqual(new(-x, -y), result);
 	}
 
 	[TestMethod]
@@ -171,28 +141,15 @@ public class PointTests
 		Assert.AreEqual(p1, result);
 	}
 
-	[TestMethod]
-	public void Multiplication_ReturnsCorrectProduct()
+	[DataTestMethod]
+	[DataRow(2, 5, 3)]
+	[DataRow(-2, -5, 3)]
+	[DataRow(2, 5, -3)]
+	public void Multiplication_ReturnsCorrectProduct(double x, double y, double scalar)
 	{
-		Point p1 = new(2, 5);
-		Point result = p1 * 3;
-		Assert.AreEqual(new(6, 15), result);
-	}
-
-	[TestMethod]
-	public void Multiplication_HandlesNegativeCoordinatesProperly()
-	{
-		Point p1 = new(-2, -5);
-		Point result = p1 * 3;
-		Assert.AreEqual(new(-6, -15), result);
-	}
-
-	[TestMethod]
-	public void Multiplication_HandlesNegativeScalarProperly()
-	{
-		Point p1 = new(2, 5);
-		Point result = p1 * -3;
-		Assert.AreEqual(new(-6, -15), result);
+		Point p1 = new(x, y);
+		Point result = p1 * scalar;
+		Assert.AreEqual(new(x * scalar, y * scalar), result);
 	}
 
 	[TestMethod]
@@ -203,28 +160,15 @@ public class PointTests
 		Assert.AreEqual(Point.Origin, result);
 	}
 
-	[TestMethod]
-	public void Division_ReturnsCorrectQuotient()
+	[DataTestMethod]
+	[DataRow(20, 15, 5)]
+	[DataRow(-20, -15, 5)]
+	[DataRow(20, 15, -5)]
+	public void Division_ReturnsCorrectQuotient(double x, double y, double scalar)
 	{
-		Point p1 = new(20, 15);
-		Point result = p1 / 5;
-		Assert.AreEqual(new(4, 3), result);
-	}
-
-	[TestMethod]
-	public void Division_HandlesNegativeCoordinatesProperly()
-	{
-		Point p1 = new(-20, -15);
-		Point result = p1 / 5;
-		Assert.AreEqual(new(-4, -3), result);
-	}
-
-	[TestMethod]
-	public void Division_HandlesNegativeScalarProperly()
-	{
-		Point p1 = new(20, 15);
-		Point result = p1 / -5;
-		Assert.AreEqual(new(-4, -3), result);
+		Point p1 = new(x, y);
+		Point result = p1 / scalar;
+		Assert.AreEqual(new(x / scalar, y / scalar), result);
 	}
 
 	[TestMethod]
@@ -236,31 +180,16 @@ public class PointTests
 	#endregion
 
 	#region Method Tests
-	[TestMethod]
-	public void DistanceToPoint_ReturnsCorrectDistance()
+	[DataTestMethod]
+	[DataRow(2, 6, 6, 9, 5)]
+	[DataRow(3, -2, -1, 1, 5)]
+	[DataRow(0, -2, 0, 3, 5)]
+	public void DistanceToPoint_ReturnsCorrectDistance(double x1, double y1, double x2, double y2, double expected)
 	{
-		Point p1 = new(2, 6);
-		Point p2 = new(6, 9);
+		Point p1 = new(x1, y1);
+		Point p2 = new(x2, y2);
 		double distance = p1.Distance(p2);
-		Assert.AreEqual(5, distance);
-	}
-
-	[TestMethod]
-	public void DistanceToPoint_ReturnsCorrectDistance_WhenPointsHaveDifferentSigns()
-	{
-		Point p1 = new(3, -2);
-		Point p2 = new(-1, 1);
-		double distance = p1.Distance(p2);
-		Assert.AreEqual(5, distance);
-	}
-
-	[TestMethod]
-	public void DistanceToPoint_ReturnsCorrectDistance_WhenPointsAreOnAxes()
-	{
-		Point p1 = new(0, -2);
-		Point p2 = new(0, 3);
-		double distance = p1.Distance(p2);
-		Assert.AreEqual(5, distance);
+		Assert.AreEqual(expected, distance);
 	}
 
 	[TestMethod]
@@ -272,31 +201,16 @@ public class PointTests
 		Assert.AreEqual(d1, d2);
 	}
 
-	[TestMethod]
-	public void GridDistToPoint_ReturnsCorrectDistance()
+	[DataTestMethod]
+	[DataRow(3, 1, 8, 5, 9)]
+	[DataRow(3, 4, -1, -1, 9)]
+	[DataRow(6, 0, 0, -3, 9)]
+	public void GridDistToPoint_ReturnsCorrectDistance(double x1, double y1, double x2, double y2, double expected)
 	{
-		Point p1 = new(3, 1);
-		Point p2 = new(8, 5);
+		Point p1 = new(x1, y1);
+		Point p2 = new(x2, y2);
 		double distance = p1.GridDist(p2);
-		Assert.AreEqual(9, distance);
-	}
-
-	[TestMethod]
-	public void GridDistToPoint_ReturnsCorrectDistance_WhenPointsHaveDifferentSigns()
-	{
-		Point p1 = new(3, 4);
-		Point p2 = new(-1, -1);
-		double distance = p1.GridDist(p2);
-		Assert.AreEqual(9, distance);
-	}
-
-	[TestMethod]
-	public void GridDistToPoint_ReturnsCorrectDistance_WhenPointsAreOnAxes()
-	{
-		Point p1 = new(6, 0);
-		Point p2 = new(0, -3);
-		double distance = p1.GridDist(p2);
-		Assert.AreEqual(9, distance);
+		Assert.AreEqual(expected, distance);
 	}
 
 	[TestMethod]
